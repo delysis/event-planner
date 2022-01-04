c07274a9d7086d6d95350b7e4fd2de35c194bcd5
Claire List <claire.list2501@gmail.com>
2022-01-04 10:08:01 -0700

    set up basic routes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import CreateEvent from './components/CreateEvent';
 import Events from './components/Events';
 import Login from './components/Login';
 
+import {Route, Link} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <h1>I am the app component! It's empty, just like my creator :=( </h1>
-      <Login />
+
+        <Link to='/login'>Click on ME to log IN!</Link>
+        <Link to='/events'>Click on ME to view EVENTS!</Link>
+        
+        <Route path='/login'>
+        <Login />
+        </Route>
+      
       <Event />
       <EditEvent />
       <CreateEvent />

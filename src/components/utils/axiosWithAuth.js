99dd633687e995971bf6a3adb53d67de667b00fb
Claire List <claire.list2501@gmail.com>
2022-01-06 13:55:16 -0700

    current cors issue on firefox

--- src/components/utils/axiosWithAuth.js
+++ src/components/utils/axiosWithAuth.js
@@ -7,7 +7,7 @@ const axiosWithAuth = ()=> {
         headers: {
             authorization: token
         },
-        baseURL: "https://forms.googleapis.com"
+        baseURL: "https://docs.google.com/forms/u/0/d/1JC-zABOglm4U7-uHkJstAqvpBzEMzAO8ToGH1Y7661I/aggregatestatistics?sid=54de8231dd925535&vc=0&c=0&w=1&flr=0&token=hr48MX4BAAA.bqXz99bgS0mSM9BPt_K7mA.pPf1zbNJxp7eH9_NpB1q7Q"
     });
 }
 

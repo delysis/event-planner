232afd9d061f5a0a1562f4bea0baba0dc4fcca95
Claire List <claire.list2501@gmail.com>
2022-01-09 12:24:47 -0700

    last push to this branch

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,11 +5,13 @@
 import React, {useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 //LOGIC section
  const Events=(props)=>{
      //DATA section
      const [date,setDate] = useState(new Date());
+     
 
      //LOGIC section
 
@@ -18,7 +20,7 @@ import Calendar from 'react-calendar';
         <div className='events-list'>
              <h1 className='text-center'>React Calendar</h1>
       <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
+        <Calendar onChange={setDate} value={props.date} />
       </div>
       <p className='text-center'>
         <span className='bold'>Selected Date:</span>{' '}
@@ -31,4 +33,4 @@ import Calendar from 'react-calendar';
 
 //RETURN section
 export default Events;
-//needful:: will do a map over each event, rendering a link to each in the above Link comp!
+//we will be passing the date to both create event and singular event page!
